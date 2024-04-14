bcbc65a4930b38073237ed724046400a4528e7c3
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-04-14 10:00:35 -0300

    Adicionei test do funcionarios_mapper

---
+++ tests/test_funcionario_mapper.py
@@ -0,0 +1,71 @@
+import unittest
+from model.funcionario_mapper import FuncionarioMapper
+from model.funcionario import Funcionario
+
+class TestFuncionarioMapper(unittest.TestCase):
+	def test_select(self):
+		funcionarioDB = FuncionarioMapper()
+		funcionarios = funcionarioDB.listar()
+		self.assertTrue(type(funcionarios) == list)
+
+		count = 0
+		print()
+		for funcionario in funcionarios:
+			if count == 1:
+				break
+			print("------------")
+			print(funcionario.cpf)
+			print(funcionario.nome)
+			print(funcionario.permissoes)
+			print("-------------")
+			count += 1
+		print()
+
+	def test_insert(self):
+		funcionarioDB = FuncionarioMapper()
+		funcionario = Funcionario()
+		funcionario.nome = "a"
+
+		funcionarioId = funcionarioDB.criar(funcionario)
+		self.assertTrue(funcionarioId)
+
+		funcionarioNoBanco = funcionarioDB.listarWhereId(id=funcionarioId)
+		self.assertEqual(funcionario.nome, funcionarioNoBanco.nome)
+
+		#Remover influencia no bd
+		funcionarioDB.deletar(id=funcionarioId)
+
+	def test_update(self):
+		funcionarioDB = FuncionarioMapper()
+		funcionario = Funcionario()
+		funcionario.nome = "A" 
+
+		funcionarioId = funcionarioDB.criar(funcionario)
+		
+		funcionario.id = funcionarioId
+		funcionario.nome = "B"
+
+		funcionarioDB.atualizar(funcionario)
+
+		funcionarioNoBanco = funcionarioDB.listarWhereId(funcionario=funcionario)
+		self.assertEqual(funcionario.nome, funcionarioNoBanco.nome)
+		
+
+		#Remover influencia no bd
+		funcionarioDB.deletar(funcionario=funcionario)
+
+	def test_delete(self):
+		funcionarioDB = FuncionarioMapper()
+		funcionario = Funcionario()
+
+		funcionarioId = funcionarioDB.criar(funcionario)
+		self.assertTrue(funcionarioId)
+
+		funcionarioDB.deletar(id=funcionarioId)
+
+		funcionario = funcionarioDB.listarWhereId(id=funcionarioId)
+		self.assertEqual(funcionario, None)
+		
+
+if __name__ == '__main__':
+	unittest.main()
