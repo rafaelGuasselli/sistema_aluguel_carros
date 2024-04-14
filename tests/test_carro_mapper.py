0636989cf8f3b2eb42ef898bdc00075addde883b
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-04-14 20:33:25 -0300

    Teste carro mapper refatorado

--- tests/test_carro_mapper.py
+++ tests/test_carro_mapper.py
@@ -1,65 +1,55 @@
+import random
+import string
 import unittest
-from model.carro_mapper import CarroMapper
 from model.carro import Carro
+from model.carro_mapper import CarroMapper
 
 class TestCarroMapper(unittest.TestCase):
+	def __init__(self, *args, **kwargs):
+		super(TestCarroMapper, self).__init__(*args, **kwargs)
+		self.carroMapper = CarroMapper()
+
 	def test(self):
 		print("Hello test!")
 		print()
 
 	def test_select(self):
-		carroDB = CarroMapper()
-		carros = carroDB.listar()
-		self.assertTrue(type(carros) == list)
-
-		count = 0
-		print()
-		for carro in carros:
-			if count == 1:
-				break
-			print("------------")
-			print(carro.cor)
-			print(carro.modelo)
-			print(carro.taxa_dia)
-			print("-------------")
-			count += 1
-		print()
+		carros = self.carroMapper.listar()
+		self.assertIsInstance(carros, list)
+		if len(carros) > 0:
+			self.assertIsInstance(carros[0], Carro)
 
 	def test_insert(self):
-		carroDB = CarroMapper()
 		carro = Carro()
-		carro.modelo = "A"
-		carro.estimativa_devolucao = None
+		carro.placa = self.criarStringAleatoria(7)
 
-		carroId = carroDB.criar(carro)
-		self.assertTrue(carroId)
-
-		carro2 = carroDB.listarWhereId(id=carroId)
-		self.assertEqual(carro.modelo, carro2.modelo)
+		carro.id = self.carroMapper.criar(carro)
+		carroNoBanco = self.carroMapper.listarWhereId(id=carro.id)
+		 
+		self.assertEqual(carro.placa, carroNoBanco.placa)
 
 	def test_update(self):
-		carroDB = CarroMapper()
 		carro = Carro()
-		carro.modelo = "A"
 
-		carroId = carroDB.criar(carro)
+		carro.placa = self.criarStringAleatoria(7)
+		carro.id = self.carroMapper.criar(carro)
+
+		carro.placa = self.criarStringAleatoria(7)
+		self.carroMapper.atualizar(carro)
 		
-		carro.id = carroId
-		carro.modelo = "B"
-
-		carroDB.atualizar(carro)
-
-		carroNoBanco = carroDB.listarWhereId(carro=carro)
-		self.assertEqual(carro.modelo, carroNoBanco.modelo)
+		carroNoBanco = self.carroMapper.listarWhereId(id=carro.id)
+		self.assertEqual(carro.placa, carroNoBanco.placa)
 
 	def test_delete(self):
-		carroDB = CarroMapper()
 		carro = Carro()
+		carro.placa = self.criarStringAleatoria(7)
 
-		carroId = carroDB.criar(carro)
-		self.assertTrue(carroId)
-
-		carroDB.deletar(id=carroId)
-
-		carro = carroDB.listarWhereId(id=carroId)
+		carro.id = self.carroMapper.criar(carro)
+		self.carroMapper.deletar(id=carro.id)
+		
+		carro = self.carroMapper.listarWhereId(id=carro.id)
 		self.assertEqual(carro, None)
+		
+
+	def criarStringAleatoria(self, tamanho):
+		return ''.join(random.choices(string.ascii_uppercase + string.digits, k=tamanho))
