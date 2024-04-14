69604c3d65f8067eb7a0cb89cc17af2f5901cd30
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-04-14 20:15:10 -0300

    Removi o gerador aleatorio

--- model/carro.py
+++ model/carro.py
@@ -1,12 +1,9 @@
-import random
-import string
-
 class Carro:
 	def __init__(self):
 		self.id = 0
 		self.cor = ""
 		self.multa = 0
-		self.placa = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+		self.placa = ""
 		self.modelo = ""
 		self.taxa_dia = 0
 		self.cliente_id = None
