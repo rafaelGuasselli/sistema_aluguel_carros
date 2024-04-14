d783f63bfedfb27f577682eb8b4f55c648db772b
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-04-14 20:25:10 -0300

    Removi as geraçoes aleatorias

--- model/cliente.py
+++ model/cliente.py
@@ -1,7 +1,5 @@
-import random
-import string
 class Cliente:
 	def __init__(self):
 		self.id = None
-		self.cpf = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+		self.cpf = ""
 		self.nome = ""
--- model/funcionario.py
+++ model/funcionario.py
@@ -1,8 +1,6 @@
-import random
-import string
 class Funcionario:
 	def __init__(self):
 		self.id = None
-		self.cpf = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+		self.cpf = ""
 		self.nome = ""
 		self.permissoes = 0
