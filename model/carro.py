6988ca3a0ac3d3efe3d971b0f79ffd37740bb498
rafaelguasselli <rafael.g.l.silva@gmail.com>
2024-04-14 19:38:04 -0300

    Eu fiz o carro gerar uma placa aleatoria

--- model/carro.py
+++ model/carro.py
@@ -1,9 +1,12 @@
+import random
+import string
+
 class Carro:
 	def __init__(self):
 		self.id = 0
 		self.cor = ""
 		self.multa = 0
-		self.placa = ""
+		self.placa = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
 		self.modelo = ""
 		self.taxa_dia = 0
 		self.cliente_id = None
